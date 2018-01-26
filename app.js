42609da272abc4d2b65f3a33876d810cb1410f24
Tom LaChance <guitarlanguageseries@gmail.com>
2018-01-25 18:51:29 -0600

    trying to get military time format

--- app.js
+++ app.js
@@ -50,10 +50,25 @@ $("#add-train-btn-submit").on("click", function(event) {
     $("#frequency-input").val("");
 });
 
-// 3. Create Firebase event for adding employee to the database and a row in the html when a user adds an entry
+// 3. Create Firebase event for adding a train to the database and a row in the html when a user adds an entry
 db.ref().on("child_added", function(childSnapshot, prevChildKey) {
 
   console.log(childSnapshot.val());
+
+  // Store everything into a variable.
+  var trainName = childSnapshot.val().$trainName;
+  var destination = childSnapshot.val().$destination;
+  var firstTrainTime = childSnapshot.val().$firstTrainTime;
+  var frequency = childSnapshot.val().$frequencyInput;
+
+  console.log(trainName);
+  console.log(destination);
+  console.log(firstTrainTime);
+  console.log(frequency);
+
+  var militaryTime = moment.unix(firstTrainTime).format("MM:mm:ss");
+  console.log(militaryTime);
+
 
 });
 
